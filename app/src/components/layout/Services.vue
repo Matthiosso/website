521fa00f6975743c79170b9f510cf667123256d3
Matthiosso <matthieuclement2012@gmail.com>
2025-08-30 13:24:29 +0200

    fix services icon

--- app/src/components/layout/Services.vue
+++ app/src/components/layout/Services.vue
@@ -7,11 +7,11 @@
                 <div class="mt-[30px] relative flex flex-col justify-center text-center rounder-[30px] border border-secondary dark:bg-[#ffffff29] bg-primary"
                     data-aos="fade-up" :data-aos-delay="element.id * 100">
                     <div class="mt-[-30px] flex justify-center relative">
-                        <div class="mt-[-7px] p-[20px] relative bg-background rounded-[50%]">
+                        <div class="mt-[-7px] p-[20px] relative rounded-[50%]">
                             <span
-                                class="h-[60px] w-[60px] m-auto border-[1px] border-solid border-secondary absolute top-[8px] left-[8px] rounded-[50%] inline-block z-[5] ">
+                                class="h-[60px] w-[60px] m-auto border-[1px] border-solid border-secondary absolute top-[8px] left-[8px] rounded-[50%] inline-block z-[5] bg-background">
                                 <Icon :icon="element.icon" alt="services"
-                                    class="h-[35px] w-[35px] relative z-[1] text-secondary" />
+                                    class="ml-1 mt-1 h-[50px] w-[50px] relative z-[1] text-secondary" />
                             </span>
                         </div>
                     </div>
@@ -35,33 +35,45 @@ import SectionHeader from '@/components/UI/SectionHeader.vue';
 const services = ref([
     {
         id: 1,
-        icon: 'majesticons:airplane-line',
-        name: 'Web development',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        icon: 'majesticons:eye-line',
+        name: 'Frontend Web development',
+        description: 'Vue.js, React.js, Tailwindcss, Vite'
     },
     {
         id: 2,
-        icon: 'majesticons:bookmark-minus-line',
-        name: 'Web development',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        icon: 'majesticons:server-line',
+        name: 'Backend Web development',
+        description: 'Spring Framework (Java), Django (Python)'
     },
     {
         id: 3,
-        icon: 'majesticons:camera-line',
-        name: 'Web development',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        icon: 'majesticons:calculator-line',
+        name: 'Software Development',
+        description: 'Qt/OpenCV (C++)'
     },
     {
         id: 4,
-        icon: 'majesticons:checkbox-list-detail',
-        name: 'Web development',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        icon: 'majesticons:presentation-chart-line',
+        name: 'Data Visualization',
+        description: 'Kibana, Jupyter/Pandas (Python)'
     },
     {
         id: 5,
-        icon: 'majesticons:covid-line',
-        name: 'Web development',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        icon: 'majesticons:box-line',
+        name: 'DevOps',
+        description: 'Docker, Mesos/Marathon, Kubernetes'
+    },
+    {
+        id: 6,
+        icon: 'majesticons:data-line',
+        name: 'Database',
+        description: 'PostgreSQL, ElasticSearch'
+    },
+    {
+        id: 7,
+        icon: 'majesticons:chats-2-line',
+        name: 'Project Management',
+        description: 'JIRA, Confluence'
     },
 ])
 </script>
