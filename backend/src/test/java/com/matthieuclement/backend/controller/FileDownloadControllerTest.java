4c9bcfbfd21f0303b799e297dbc3514270a71102
Matthiosso <matthieuclement2012@gmail.com>
2025-09-06 16:10:21 +0200

    backend: fix tests with fake resume file

--- backend/src/test/java/com/matthieuclement/backend/controller/FileDownloadControllerTest.java
+++ backend/src/test/java/com/matthieuclement/backend/controller/FileDownloadControllerTest.java
@@ -1,19 +1,23 @@
 package com.matthieuclement.backend.controller;
 
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import java.nio.charset.StandardCharsets;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = {
+        "file.resume-filepath=./src/test/resources/fake-file.txt"
+})
 public class FileDownloadControllerTest {
-
     @Autowired
     private TestRestTemplate template;
 
@@ -21,6 +25,7 @@ public class FileDownloadControllerTest {
     public void testGetResumeFile() throws Exception {
         ResponseEntity<Resource> response = template.getForEntity("/download/resume", Resource.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(200));
-        assertThat(response.getBody().getFilename().equals("resume.pdf"));
+        assertThat(response.getBody().getFilename()).isEqualTo("fake-file.txt");
+        assertThat(response.getBody().getContentAsString(StandardCharsets.UTF_8)).isEqualTo("Test");
     }
 }
--- backend/src/test/java/com/matthieuclement/backend/service/FileDownloadServiceTest.java
+++ backend/src/test/java/com/matthieuclement/backend/service/FileDownloadServiceTest.java
@@ -3,13 +3,18 @@ package com.matthieuclement.backend.service;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.Resource;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "file.resume-filepath=./src/test/resources/fake-file.txt"
+})
 public class FileDownloadServiceTest {
 
     @Autowired
@@ -19,5 +24,7 @@ public class FileDownloadServiceTest {
     public void testLoadFileAsResource() throws IOException {
         Resource res = fileDownloadService.loadFileAsResource();
         assertThat(res.exists());
+        assertThat(res.getFilename().equals("fake-file.txt"));
+        assertThat(res.getContentAsString(StandardCharsets.UTF_8).equals("Test"));
     }
 }
