c813627cae9c0f3ec50c27777d57012a89c80f1e
Matthiosso <matthieuclement2012@gmail.com>
2025-09-07 16:55:44 +0200

    frontend: refactor meta tag settings

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -25,15 +25,13 @@ const title = 'Matthieu CLEMENT: My portfolio website'
 const description = "Hey, I'm Matthieu CLEMENT, a french developer. Here's my portfolio website. Looking for a full stack developer or technical consultant ? I'm your man 👨‍💻"
 
 useSeoMeta({
-  // title & descriptions
-  ogTitle: title,
-  ogLocale: 'en_US',
-  ogType: 'website',
-  ogDescription: description,
-  twitterTitle: title,
-  twitterDescription: description,
+  // Basic SEO
+  title: title,
+  description: description,
 
-  // og image
+  // Open Graph
+  ogTitle: title,
+  ogDescription: description,
   ogImage: {
     url: imgUrl,
     width: 1200,
@@ -41,6 +39,13 @@ useSeoMeta({
     alt: 'Logo',
     type: 'image/png'
   },
+  ogType: 'website',
+  ogSiteName: 'Matthieu CLEMENT',
+  ogLocale: 'en_US',
+
+  // Twitter
+  twitterTitle: title,
+  twitterDescription: description,
   twitterImage: {
     url: imgUrl,
     width: 1200,
@@ -48,8 +53,8 @@ useSeoMeta({
     alt: 'Logo',
     type: 'image/png'
   },
-  // twitter image (note: ogImage is used as a fallback so this is optional)
-  twitterCard: 'summary_large_image', // or summary
+  twitterCard: 'summary_large_image',
+
 })
 
 </script>
