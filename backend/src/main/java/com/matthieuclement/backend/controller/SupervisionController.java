c87903f78ceba3490f6fd6c12f8cb1b97bd57665
Matthiosso <matthieuclement2012@gmail.com>
2025-09-11 19:19:48 +0200

    backend: add version api

--- backend/src/main/java/com/matthieuclement/backend/controller/SupervisionController.java
+++ backend/src/main/java/com/matthieuclement/backend/controller/SupervisionController.java
@@ -2,6 +2,7 @@ package com.matthieuclement.backend.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +12,18 @@ public class SupervisionController {
 
     private static final Logger logger = LoggerFactory.getLogger(SupervisionController.class);
 
+    @Value("${application.version}")
+    private String applicationVersion;
+
     @GetMapping("/health")
     public ResponseEntity<String> healthCheck() {
         logger.info("Checking health");
         return ResponseEntity.ok("OK");
     }
+
+    @GetMapping("version")
+    public ResponseEntity<String> getVersion() {
+        return ResponseEntity.ok(applicationVersion);
+    }
+
 }
