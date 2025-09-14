c05dd73a7c10e1a633058253c37d94788ca0d0f3
Matthiosso <matthieuclement2012@gmail.com>
2025-09-14 17:07:33 +0200

    test(ci): try semantic release on maven project

--- backend/release.config.js
+++ backend/release.config.js
@@ -3,7 +3,9 @@ export default {
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
-        'semantic-release-maven'
-    ]
+        '@semantic-release/npm',
+        '@semantic-release/github',
+        '@terrestris/maven-semantic-release'
+    ],
 
 };
