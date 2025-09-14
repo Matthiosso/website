522f76403f693a42c641b2d84a40add0e5fac1d6
Matthiosso <matthieuclement2012@gmail.com>
2025-09-14 21:21:11 +0200

    ci(frontend): add git and prerelease for semver

--- frontend/release.config.js
+++ frontend/release.config.js
@@ -1,3 +1,17 @@
 export default { 
-    branches: ['main']
+    branches: [
+        { name: 'main' },
+        { name: 'dev', channel: 'dev', prerelease: true }
+    ],
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/changelog',
+        '@semantic-release/npm',
+        ['@semantic-release/git', {
+            'assets': ['dist/**/*.{js,css}', 'docs', 'package.json'],
+            'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+        }],
+        '@semantic-release/github'
+    ]
 };
