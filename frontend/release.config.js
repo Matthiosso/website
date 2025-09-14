9a4bca89ae4368303b20bd9a8e0c099d318dd4ee
Matthiosso <matthieuclement2012@gmail.com>
2025-09-14 11:42:13 +0200

    test(ci): pushing docker using npx semantic release

--- frontend/release.config.js
+++ frontend/release.config.js
@@ -1,3 +1,28 @@
 export default { 
     branches: ['main', 'dev'],
+    plugins: [
+        [
+            '@codedependant/semantic-release-docker', 
+            {
+                dockerTags: ['latest', '{{version}}', '{{major}}-latest', '{{major}}.{{minor}}', '{{major}}.{{minor}}.{{patch}}'],
+                dockerFile: 'Dockerfile',
+                dockerPlatform: ['linux/amd64', 'linux/arm64'],
+                dockerBuildFlags: {
+                    pull: null
+                , target: 'release'
+                },
+                dockerArgs: {
+                    API_TOKEN: null, 
+                    RELEASE_DATE: new Date().toISOString(), 
+                    RELEASE_VERSION: '{{next.version}}'
+                },
+                dockerLabels: {
+                    'org.opencontainers.image.title': 'Portfolio Website Frontend',
+                    'org.opencontainers.image.description': 'Portfolio Website Frontend using Vue.js and Vite',
+                    'org.opencontainers.image.version': '{{next.version}}',
+                    'org.opencontainers.image.created': new Date().toISOString(),
+                }
+            }
+        ]
+    ]
 };
