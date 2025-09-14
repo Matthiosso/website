6fda873644041fcf0785f1d2a2d22ab77dc6c765
Matthiosso <matthieuclement2012@gmail.com>
2025-09-14 11:59:58 +0200

    test(ci): testing config for docker registry

--- frontend/release.config.js
+++ frontend/release.config.js
@@ -9,22 +9,22 @@ export default {
                 dockerFile: 'Dockerfile',
                 dockerRegistry: 'ghcr.io',
                 dockerProject: 'matthiosso/website',
-                dockerPlatform: ['linux/amd64', 'linux/arm64'],
+                dockerPlatform: ['linux/amd64'],
                 dockerBuildFlags: {
-                    pull: null
-                , target: 'release'
+                    pull: null, 
+                    target: 'release',
+                    labels: {
+                        'org.opencontainers.image.title': 'Portfolio Website Frontend',
+                        'org.opencontainers.image.description': 'Portfolio Website Frontend using Vue.js and Vite',
+                        'org.opencontainers.image.version': '{{next.version}}',
+                        'org.opencontainers.image.created': new Date().toISOString(),
+                    }
                 },
                 dockerArgs: {
                     API_TOKEN: null, 
                     RELEASE_DATE: new Date().toISOString(), 
                     RELEASE_VERSION: '{{next.version}}'
                 },
-                dockerLabels: {
-                    'org.opencontainers.image.title': 'Portfolio Website Frontend',
-                    'org.opencontainers.image.description': 'Portfolio Website Frontend using Vue.js and Vite',
-                    'org.opencontainers.image.version': '{{next.version}}',
-                    'org.opencontainers.image.created': new Date().toISOString(),
-                }
             }
         ]
     ]
