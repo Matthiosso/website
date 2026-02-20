27a0892d0631fb493d951506eb3e7f9eea26faf3
Matthiosso <matthieuclement2012@gmail.com>
2026-02-20 10:47:48 -0700

    fix(frontend): add env var to index.html

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -10,9 +10,7 @@
 </template>
 
 <script setup>
-import { computed, defineAsyncComponent, ref } from 'vue';
-import { useSeoMeta } from '@unhead/vue';
-import img from '/logo/MC_1200x630.png';
+import { defineAsyncComponent } from 'vue';
 
 
 const Navbar = defineAsyncComponent(() => import('@/components/layout/Navbar.vue'));
@@ -21,45 +19,5 @@ const Counter = defineAsyncComponent(() => import('@/components/layout/Counter.v
 const Services = defineAsyncComponent(() => import('@/components/layout/Services.vue'));
 const Contact = defineAsyncComponent(() => import('@/components/layout/Contact.vue'));
 const Footer = defineAsyncComponent(() => import('@/components/layout/Footer.vue'));
-
-// This code is to dynamically add the meta property for url preview
-const metaInfos = ref({
-  imgUrl: new URL(img, import.meta.url).href,
-  title: 'Matthieu CLEMENT | My portfolio website',
-  description: "Hey, I'm Matthieu CLEMENT, a french developer. Here's my portfolio website. Looking for a full stack developer or technical consultant ? I'm your man 👨‍💻"
-})
-
-useSeoMeta({
-  // Basic SEO
-  title: computed(() => metaInfos.value.title),
-  description: computed(() => metaInfos.value.description),
-
-  // Open Graph
-  ogTitle: computed(() => metaInfos.value.title),
-  ogDescription: computed(() => metaInfos.value.description),
-  ogImage: {
-    url: computed(() => metaInfos.value.imgUrl),
-    width: 1200,
-    height: 600,
-    alt: 'Logo',
-    type: 'image/png'
-  },
-  ogType: 'website',
-  ogSiteName: 'Matthieu CLEMENT',
-  ogLocale: 'en_US',
-
-  // Twitter
-  twitterTitle: computed(() => metaInfos.value.title),
-  twitterDescription: computed(() => metaInfos.value.description),
-  twitterImage: {
-    url: computed(() => metaInfos.value.imgUrl),
-    width: 1200,
-    height: 600,
-    alt: 'Logo',
-    type: 'image/png'
-  },
-  twitterCard: 'summary_large_image',
-
-})
 
 </script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,22 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { createHead } from '@unhead/vue/client'
 import { Icon } from '@iconify/vue' // to find icons, visit https://icon-sets.iconify.design
 import Countup from 'vue-countup-v3';
 import AOS from 'aos'; // for nice css animations
-import { createGtag } from "vue-gtag";
-import config from './config';
 import 'aos/dist/aos.css';
 AOS.init({duration: 1000});
 
 const app = createApp(App);
-const head = createHead();
-app.use(head);
-app.use(createGtag({
-  property: {
-    id: config.gtagId || 'G-XXXXXXXXXX'
-  }
-}));
 app.component('Icon', Icon)
 app.component('Countup', Countup)
 app.mount('#app')
